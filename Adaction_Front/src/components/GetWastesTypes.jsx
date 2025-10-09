1900052c223f448dce3b8c3674fe3cedb6cffc6d
Hellyes31 <elies.auguet@gmail.com>
2025-10-09 14:53:28 +0200

    add: GetWastesTypes.jsx, works

---
+++ Adaction_Front/src/components/GetWastesTypes.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import '/src/App.css'
+
+function App() {
+  const [wastes_types, setWastes_types] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/wastetype")
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Erreur serveur : " + res.status);
+        }
+        return res.json();
+      })
+      .then(data => setWastes_types(data))
+      .catch(err => console.error("Erreur de fetch :", err));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {wastes_types.length > 0 ? (
+          wastes_types.map(w => (
+            <li key={w.id}>
+              {w.label}
+            </li>
+          ))
+        ) : (
+          <p>Aucun bénévole trouvé.</p>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- Adaction_Front/src/main.jsx
+++ Adaction_Front/src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import CreateVolunteer from './components/CreateVolunteer.jsx'
 import CreateCollect from './components/CreateCollect.jsx'
+import GetWastesTypes from './components/GetWastesTypes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CreateVolunteer/>
     <CreateCollect/>
-    <App />
+    <GetWastesTypes/>
   </StrictMode>,
 )
