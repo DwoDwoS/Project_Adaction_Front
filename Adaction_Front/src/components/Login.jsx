352e4458bfb9d15821e4c65b2b3c5c13968bc3b6
Hellyes31 <elies.auguet@gmail.com>
2025-10-10 14:36:40 +0200

    add: component Login.jsx with post method

---
+++ Adaction_Front/src/components/Login.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import '/src/App.css'
+
+function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+  //   useEffect(() => {
+  //   fetch("http://localhost:8080/api/login")
+  //     .then(res => {
+  //       if (!res.ok) {
+  //         throw new Error("Erreur serveur : " + res.status);
+  //       }
+  //       return res.json();
+  //     })
+  //     .then(data => setWastes_types(data))
+  //     .catch(err => console.error("Erreur de fetch :", err));
+  // }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const loginForm = {
+      username: username,
+      password: password
+     }
+    
+
+    console.log("Données envoyées :", JSON.stringify(loginForm));
+
+    fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginForm)
+    })
+      .then(res => {
+        if (!res.ok) throw new Error("Erreur serveur : " + res.status);
+        return res.json();
+      })
+      .then(() => console.log(`Bienvenue ${username}`))
+      .catch(err => console.error("Erreur POST :", err));
+  };
+  return (
+    <div>
+        <h2>Connexion</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Votre prénom'required value={username} onChange={(e) => setUsername(e.target.value)} />
+            <label>Prénom</label>
+            <input type="password" placeholder='Votre mot de passe'required value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button className='submit-btn'>Se connecter</button>
+            <button>Gérer les bénévoles</button>
+        </form>
+    </div>
+  )
+}
+export default Login;
