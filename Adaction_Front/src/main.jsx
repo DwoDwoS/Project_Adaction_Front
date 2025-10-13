c55bb86a3ec463bec7b33b95e37a6f383b0d115d
Hellyes31 <elies.auguet@gmail.com>
2025-10-13 13:25:55 +0200

    add: component Donation

--- Adaction_Front/src/main.jsx
+++ Adaction_Front/src/main.jsx
@@ -6,9 +6,10 @@ import './index.css'
 // import CreateCollect from './components/CreateCollect.jsx'
 // import GetWastesTypes from './components/GetWastesTypes.jsx'
 import Login from './components/Login.jsx'
+import CreateDonation from './components/CreateDonation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login/>
+    <CreateDonation/>
   </StrictMode>,
 )
