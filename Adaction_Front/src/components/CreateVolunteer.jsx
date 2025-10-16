22b0c5e6e04636df2a4b9c7eaf6692c51ee68f24
Elouan <elouangauriaud@gmail.com>
2025-10-16 15:11:06 +0200

    feat: adding a modal window for volunteer's creation

--- Adaction_Front/src/components/CreateVolunteer.jsx
+++ Adaction_Front/src/components/CreateVolunteer.jsx
@@ -1,29 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Outlet } from 'react-router'
+import { useState } from 'react'
 import '/src/App.css'
 
 function CreateVolunteer() {
-  const [volunteers, setVolunteers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [location, setLocation] = useState("");
 
-  // Charger la liste au démarrage
-  useEffect(() => {
-    fetch("http://localhost:8080/api/volunteers")
-      .then(res => {
-        if (!res.ok) {
-          throw new Error("Erreur serveur : " + res.status);
-        }
-        return res.json();
-      })
-      .then(data => setVolunteers(data))
-      .catch(err => console.error("Erreur de fetch :", err));
-  }, []);
 
-  //Requête POST
    const handleSubmit = (e) => {
     e.preventDefault();
     const volunteersForm  = { firstname, lastname, email, password, location};
@@ -38,49 +24,69 @@ function CreateVolunteer() {
       })
   };
 
+  const handleClose = () => { 
+    setIsModalOpen(false);
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPassword("");
+    setLocation("");
+  };
+
   return (
     <div className="CreateVolunteer">
-      <h1>Ajouter un.e bénévole</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Prénom</label>
-        <input 
-        type="text" 
-        required 
-        value={firstname}
-        onChange={(e)=> setFirstname(e.target.value)}
-        />
-        <label>Nom</label>
-        <input 
-        type="text" 
-        required 
-        value={lastname}
-        onChange={(e)=> setLastname(e.target.value)}
-        />
-        <label>Email</label>
-        <input 
-        type="text" 
-        required 
-        value={email}
-        onChange={(e)=> setEmail(e.target.value)}
-        />
-        <label>Mot de passe</label>
-        <input 
-        type="text" 
-        required 
-        value={password}
-        onChange={(e)=> setPassword(e.target.value)}
-        />
-        <label>Localisation</label>
-        <input 
-        type="text" 
-        required 
-        value={location}
-        onChange={(e)=> setLocation(e.target.value)}
-        />
-        <button>Ajouter</button>
-        <button>Annuler</button>
-        <p>{firstname}</p>
-      </form>
+      <button onClick= {() => setIsModalOpen(true)} className="modal-actions submit-btn">
+        Ajouter un.e bénévole
+      </button>
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={handleClose}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="CreateVolunteer modal">
+              <h3>Ajouter un.e bénévole</h3>
+              <form onSubmit={handleSubmit}>
+                <label>Prénom</label>
+                <input 
+                type="text" 
+                required 
+                value={firstname}
+                onChange={(e)=> setFirstname(e.target.value)}
+                />
+                <label>Nom</label>
+                <input 
+                type="text" 
+                required 
+                value={lastname}
+                onChange={(e)=> setLastname(e.target.value)}
+                />
+                <label>Email</label>
+                <input 
+                type="text" 
+                required 
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}
+                />
+                <label>Mot de passe</label>
+                <input 
+                type="text" 
+                required 
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)}
+                />
+                <label>Localisation</label>
+                <input 
+                type="text" 
+                required 
+                value={location}
+                onChange={(e)=> setLocation(e.target.value)}
+                />
+                <button type="submit">Ajouter</button>
+                <button type="button" onClick={handleClose}>Annuler</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
